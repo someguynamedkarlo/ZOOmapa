69e0bcb479a67d420acb18a33b4ec4528a3ca3c2
Jakov Tomasić <jakov.tomasic1@gmail.com>
2025-02-07 16:17:31 +0100

    v1.0.1 - map group click threshold increased a bit

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ export const MIN_ZOOM = 11;
 export const MAX_ZOOM = 19;
 export const MAX_TILE_ZOOM = 17; // Max zoom for requesting new tiles. All zooms greater that this will look more and more blurry
 
-export const LAT_CLICK_GROUP_THRESHOLD = 0.00001;
-export const LONG_CLICK_GROUP_THRESHOLD = 0.00001;
+export const LAT_CLICK_GROUP_THRESHOLD = 0.00003;
+export const LONG_CLICK_GROUP_THRESHOLD = 0.00003;
 
 // TODO: set to false
 export const DEBUG: boolean = false;
